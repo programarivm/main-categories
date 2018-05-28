a95e79102e5ed1f2e1d325dab96080cb36622722
programarivm <info@programarivm.com>
2018-05-28 21:18:10 +0100

    documenting the Main_Categories_Loader class

--- includes/class-main-categories-loader.php
+++ includes/class-main-categories-loader.php
@@ -5,23 +5,65 @@
  */
 class Main_Categories_Loader
 {
+	/**
+	 * The array of actions registered with WordPress.
+	 *
+	 * @var array $actions The actions registered with WordPress to fire when the plugin loads.
+	 */
 	protected $actions;
 
+	/**
+	 * The array of filters registered with WordPress.
+	 *
+	 * @var array $filters The filters registered with WordPress to fire when the plugin loads.
+	 */
 	protected $filters;
 
+	/**
+	 * Constructor.
+	 */
 	public function __construct() {
-    	$this->actions = array();
-    	$this->filters = array();
+		$this->actions = array();
+		$this->filters = array();
 	}
 
+	/**
+	 * Adds a new action to the collection to be registered with WordPress.
+	 *
+	 * @param string $hook The name of the WordPress action that is being registered.
+	 * @param object $component A reference to the instance of the object on which the action is defined.
+	 * @param string $callback The name of the function definition on the $component.
+	 * @param int $priority The priority at which the function should be fired.
+	 * @param int $accepted_args The number of arguments that should be passed to the $callback.
+	 */
 	public function add_action( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
 		$this->actions = $this->add( $this->actions, $hook, $component, $callback, $priority, $accepted_args );
 	}
 
+	/**
+	 * Adds a new filter to the collection to be registered with WordPress.
+	 *
+	 * @param string $hook The name of the WordPress action that is being registered.
+	 * @param object $component A reference to the instance of the object on which the action is defined.
+	 * @param string $callback The name of the function definition on the $component.
+	 * @param int $priority The priority at which the function should be fired.
+	 * @param int $accepted_args The number of arguments that should be passed to the $callback.
+	 */
 	public function add_filter( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
 		$this->filters = $this->add( $this->filters, $hook, $component, $callback, $priority, $accepted_args );
 	}
 
+	/**
+	 * Registers the actions and hooks into a single collection.
+	 *
+	 * @param array $hooks The collection of hooks that is being registered (that is, actions or filters).
+	 * @param string $hook The name of the WordPress filter that is being registered.
+	 * @param object $component A reference to the instance of the object on which the filter is defined.
+	 * @param string $callback The name of the function definition on the $component.
+	 * @param int $priority The priority at which the function should be fired.
+	 * @param int $accepted_args The number of arguments that should be passed to the $callback.
+	 * @return array The collection of actions and filters registered with WordPress.
+	 */
 	private function add( $hooks, $hook, $component, $callback, $priority, $accepted_args ) {
 		$hooks[] = array(
 			'hook'          => $hook,
@@ -34,6 +76,9 @@ class Main_Categories_Loader
 		return $hooks;
 	}
 
+	/**
+	 * Registers the filters and actions with WordPress.
+	 */
 	public function run() {
 		foreach ( $this->filters as $hook ) {
 			add_filter( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
